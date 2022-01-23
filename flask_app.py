16f43574644f2ec9b3b8a4e94a100c7e49746b6d
Valentin BUIRA <valentin.buira@gmail.com>
2022-01-23 20:38:10 +0100

    mise en place de la bd

--- flask_app.py
+++ flask_app.py
@@ -1,13 +1,71 @@
 # save this as app.py
+
+from asyncore import read
 from flask import Flask, escape, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 from markupsafe import re
+from enum import Enum
+import configparser
+
+
+from sqlalchemy import create_engine
+import sqlalchemy
+
+class EnumDevEnv(Enum): #Enivirronement de developpement
+    DEV = 1
+    PROD = 2
+
+
+dev_env = EnumDevEnv.DEV;
 
 app = Flask(__name__)
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+#app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+config = configparser.ConfigParser()
+config.read('config_db.ini')
+
+if dev_env== EnumDevEnv.PROD:
+    
+    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+        username= config["PROD"]["username"],
+        password= config["PROD"]["password"],
+        hostname= config["PROD"]["hostname"],
+        databasename= config["PROD"]["databasename"],
+    )
+elif dev_env == EnumDevEnv.DEV:
+    SQLALCHEMY_DATABASE_URI = "mariadb+mysql+pymysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+        username= config["DEV"]["username"],
+        password= config["DEV"]["password"],
+        hostname= config["DEV"]["hostname"],
+        databasename= config["DEV"]["databasename"],
+    )
+
+"""
+engine = sqlalchemy.create_engine(SQLALCHEMY_DATABASE_URI, echo=True)
+print(engine.table_names())
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://user:password@host/dbname'
+
+# Test if it works
+engine = sqlalchemy.create_engine(SQLALCHEMY_DATABASE_URI, echo=True)
+print(engine.table_names())
+"""
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+#engine = db.create_engine(SQLALCHEMY_DATABASE_URI)
 
 @app.route('/')
+@app.route('/index')
 def index():
     name = request.args.get("name", "World")
-    #return render_template("index.html",  title='Mobilites IUAR')
+    return render_template("index.html",  title='Mobilites IUAR')
     return f'Hello, {escape(name)}!'
-    
+
+@app.route('/success')
+def success():
+    return "success !"
+
